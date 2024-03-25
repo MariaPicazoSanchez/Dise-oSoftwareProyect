957d9cf3135a70b709f53ddadce6afccb3b29911
María <129367348+MariaPicazoSanchez@users.noreply.github.com>
2024-03-25 12:39:35 +0100

    cambio de nombres

--- game.py
+++ game.py
@@ -1,15 +1,19 @@
 "consola para saber como funcionan los hilos"
 import time
-from laberinto import Game,Bicho,Agresivo,Room
+from laberinto import Juego,Bicho,Agresivo,Habitacion,Personaje
 
 
-game=Game()
-game.make4Rooms4BichosFM()
-sm="pepe"
-
-game.maze.entrarAlguien(sm)
-game = Game()
-bicho = Bicho(Agresivo(), 3, 5, Room(1))
+game=Juego()
+game.fabLab4Hab4BichosFM()
+per=Personaje()
+per.nombre="Juan"
+game.laberinto.entrarAlguien(per)
+game = Juego()
+bicho = Bicho()
+bicho.modo = Agresivo()
+bicho.vidas = 3
+bicho.poder = 5
+bicho.posicion = Habitacion(1)
 game.lanzarHilo(bicho)
 time.sleep(15)
 game.terminarHilo(bicho)
