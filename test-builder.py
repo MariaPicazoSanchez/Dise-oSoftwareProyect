2e0e8eee3049516a67ad74646aa4c88b4e0c9ad3
María <129367348+MariaPicazoSanchez@users.noreply.github.com>
2024-04-21 13:07:18 +0200

    con los test del builder

--- test-builder.py
+++ test-builder.py
@@ -2,26 +2,83 @@ import unittest
 from builder import LaberintoBuilder, Director, LaberintoBuilderHexagonal
 from laberinto import Habitacion
 
-class TestLaberintoBuilder(unittest.TestCase):
+class TestLaberinto(unittest.TestCase):
     def setUp(self):
-        self.builder = LaberintoBuilder()
+        director=Director()
+        director.procesar('C:\Users\maria\Documents\2 Ing\Diseño software\DiferentesLaberintos\1erLaberinto\laberintoHex4hab4bichos.json')
+        self.dic = self.director.diccionario
+        self.juego=director.obtenerJuego()
+        self.juego.agregarPersonaje('Pepito')
 
-    def test_fabricarHabitacion(self):
-        habitacion = self.builder.fabricarHabitacion(1)
-        self.assertIsNotNone(habitacion)
-        # Agrega más aserciones para verificar otros aspectos de la creación de habitaciones
+    def comprobarArmarioEn(self, num, unCont):
+        arm = next((each for each in unCont.hijos if each.esArmario), None)
+        assert arm is not None and arm.esArmario, f"No se encontró el armario {num} en el contenedor"
+        return arm
+    def comprobarBombaEn(self, unContenedor):
+        bomba = next((each for each in unContenedor.hijos if each.es_bomba), None)
+        assert bomba is not None and bomba.esBomba, "El contenedor no contiene una bomba"
+    def comprobarHabitacion(self, num):
+        hab = self.juego.obtenerHabitacion(num)
+        self.assertEqual(hab.num, num)
+        return hab
+    def comprobarLaberintoRecursivo(self, unDic, padre):
+        nada = True
+        
+        # Contenedores
+        if unDic['tipo'] == 'habitacion':
+            nada = False
+            con = self.comprobarHabitacion(unDic['num'])
+        elif unDic['tipo'] == 'armario':
+            nada = False
+            con = self.comprobarArmario(unDic['num'], padre)
+        
+        # Hojas
+        elif unDic['tipo'] == 'bomba':
+            nada = False
+            self.comprobarBombaEn(padre)
+        elif unDic['tipo'] == 'tunel':
+            nada = False
+            self.comprobarTunelEn(padre)
+        
+        lista = unDic.get('hijos', None)
+        if lista is not None:
+            for each in lista:
+                self.comprobarLaberintoRecursivo(each, con)
+        
+        if nada:
+            self.assertFalse(True)
 
-    def test_fabricarPuertaLados(self):
-        habitacion1 = Habitacion()
-        habitacion2 = Habitacion()
-        puerta = self.builder.fabricarPuertaLados(habitacion1, habitacion2)
-        self.assertIsNotNone(puerta)
-        self.assertEqual(puerta.lado1, habitacion1)
-        self.assertEqual(puerta.lado2, habitacion2)
-        # Agrega más aserciones para verificar otros aspectos de la creación de puertas
+    def comprobarPuertaDe(self, unNum, unaOr, otroNum, otraOr):
+        unaHab = self.juego.obtenerHabitacion(unNum)
+        otraHab = self.juego.obtenerHabitacion(otroNum)
 
-    # Agrega más tests para otros métodos de LaberintoBuilder
+        self.assertEqual(unaHab.num, unNum)
+        self.assertEqual(otraHab.num, otroNum)
 
+        or1 = getattr(self.director.builder, f'fabricar{unaOr}')()
+        or2 = getattr(self.director.builder, f'fabricar{otraOr}')()
+
+        pt1 = unaHab.obtenerElemento(or1)
+        pt2 = otraHab.obtenerElemento(or2)
+
+        self.assertTrue(pt1.esPuerta())
+        self.assertTrue(pt2.esPuerta())
+        self.assertEqual(pt1, pt2)
+
+        self.assertFalse(pt1.estaAbierta())
+
+    def comprobarTunelEn(self, unContenedor):
+        tunel = next((each for each in unContenedor.hijos if each.es_tunel), None)
+        assert tunel is not None and tunel.esTunel, "El contenedor no contiene un túnel"
+    
+    def testLaberinto(self):
+        for each in self.dic['laberinto']:
+            self.comprobarLaberintoRecursivo(each, 'root')
+
+        if 'puertas' in self.dic:
+            for cada in self.dic['puertas']:
+                self.comprobarPuertaDe(cada[0], cada[1], cada[2], cada[3])
+                
 class TestDirector(unittest.TestCase):
     def setUp(self):
         self.director = Director()
